33d269e956930db3b91893c4e89a8763e92f4a18
Bhadan <bhadanpaul@gmail.com>
2026-04-16 23:13:16 +0600

    Prictic flutter widget

--- lib/alert.dart
+++ lib/alert.dart
@@ -7,14 +7,106 @@ class alert extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screen = MediaQuery.of(context).size;
     void showAlertDialog(){
       showDialog(context: context, builder: (context)=>AlertDialog(
         title: Text('Alert👽👽👽👽👽'),
         content: Text('Are you sure ....'),
         actions: [
-          TextButton(onPressed: () {}, child: Text('Cancle')),
+          TextButton(onPressed: () {
+            Navigator.pop(context);
+          }, child: Text('Cancle')),
           ElevatedButton(onPressed: () {}, child: Text('Ok')),
         ],
+      ));
+
+    }
+    //showDialogBox
+
+    void showDialogBox(){
+      showDialog(context: context, builder: (context)=> AlertDialog(
+        title: Text('Alert👽👽👽👽👽'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Icon(Icons.add_alert,color: Colors.red,),
+                SizedBox(width: 10,),
+                Text('Alert👽👽👽👽👽',style: TextStyle(color: Colors.red,fontSize: 20,fontWeight: FontWeight.bold),),
+
+              ],
+            ),
+            SizedBox(width: 10,),
+            Text('Are you sure ....',style: TextStyle(color: Colors.red,fontSize: 20,fontWeight: FontWeight.bold),),
+
+          ],
+        ),
+        actions: [
+          TextButton(onPressed: () {
+            Navigator.pop(context);
+          }, child: Text('No')),
+          ElevatedButton(onPressed: () {
+            Navigator.pop(context);
+          }, child: Text('Yes')),
+        ],
+      ));
+
+    }
+    //SimpleDialogBox
+    void SimpleDialogBox(){
+      showDialog(context: context,
+          barrierDismissible: false,
+          builder:
+          (context)=> SimpleDialog(
+        title: Text('Alert👽👽👽👽👽'),
+        children: [
+          SimpleDialogOption(
+            child: Text('Option 1'),
+
+          ),
+          SimpleDialogOption(
+            child: Text('Option 2'),
+          ),
+          SimpleDialogOption(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter Your number'
+              ),
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(onPressed: () {
+              Navigator.pop(context);
+            }, child: Text('Ok')),
+          )
+        ],
+
+      ));
+    }
+    //showBottomSheetAlert Box
+    void showBottomSheetAlert(){
+      showModalBottomSheet(context: context, builder: (context)=> Container(
+        child: Column(
+          children: [
+
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            SizedBox(height: 20,)
+            ,
+            ElevatedButton(onPressed: () {
+              Navigator.pop(context);
+            }, child: Text('Close')),
+
+          ],
+        ),
       ));
     }
     return Scaffold(
@@ -23,7 +115,29 @@ class alert extends StatelessWidget {
       ),
       body: Column(
         children: [
-          ElevatedButton(onPressed: () {}, child: Text('Alert Box')),
+          Container(
+            color: Colors.red,
+            height: screen.height*0.5,
+            width: screen.width*0.3,
+          ),
+          SizedBox(height: 20,),
+          Text('Alert Box',style: TextStyle(fontSize: screen.width> 600 ?32:18,),),
+          ElevatedButton(onPressed: () {
+            showAlertDialog();
+          }, child: Text('Alert Box')),
+          //Show dialog box
+          ElevatedButton(onPressed: () {
+            showDialogBox();
+          }, child: Text('show dialog Box')),
+         //Simple Dialog Box
+          ElevatedButton(onPressed: () {
+            SimpleDialogBox();
+          }, child: Text('Simple Dialog Box`')),
+          //showBottomSheetAlert
+          ElevatedButton(onPressed: () {
+            showBottomSheetAlert();
+          }, child: Text('showBottomSheetAlert')),
+
         ],
       ),
     );
